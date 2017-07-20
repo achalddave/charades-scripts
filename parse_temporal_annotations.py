f325585d0bc17c2b0b5468d94e1e0d413f1853c2
Achal Dave <achalddave@live.com>
2017-07-20 03:41:20 -0400

    Move some code to helper parsing.py

--- parse_temporal_annotations.py
+++ parse_temporal_annotations.py
@@ -26,57 +26,16 @@ import re
 from math import ceil, floor
 from os import path
 
+from parsing import parse_annotations
 
-def main(input_annotations_path, charades_classes_path, video_frames_info_path,
+
+def main(input_annotations_path, video_frames_info_path, charades_classes_path,
          output):
-    class_mapping = {}
-    with open(charades_classes_path) as f:
-        # Lines are of the form '<class_id> <class_description>'
-        class_matcher = re.compile('([^\s]*) (.*)')
-        for line in f:
-            line = line.strip()
-            class_id, class_description = class_matcher.match(line).groups()
-            class_mapping[class_id] = class_description
-
-    video_fps = {}
-    with open(video_frames_info_path) as f:
-        video_frames_info_reader = csv.DictReader(f)
-        for row in video_frames_info_reader:
-            video_fps[row['video']] = float(row['fps'])
-
-    annotations = []
-    with open(input_annotations_path) as f:
-        annotations_reader = csv.DictReader(f)
-        for row in annotations_reader:
-            if not row['actions']:
-                continue
-            video = row['id']
-            subject = row['subject']
-            quality = int(row['quality']) if row['quality'] else -1
-            verified = True if row['verified'] == 'Yes' else False
-            action_instances = row['actions'].split(';')
-            frames_per_second = video_fps[video]
-            for instance in action_instances:
-                class_id, start_seconds, end_seconds = instance.split(' ')
-                start_seconds = float(start_seconds)
-                end_seconds = float(end_seconds)
-                start_frame = floor(start_seconds * frames_per_second)
-                end_frame = ceil(end_seconds * frames_per_second)
-                annotations.append({
-                    'filename': video,
-                    'subject': subject,
-                    'verified': verified,
-                    'quality': quality,
-                    'start_seconds': start_seconds,
-                    'end_seconds': end_seconds,
-                    'start_frame': start_frame,
-                    'end_frame': end_frame,
-                    'frames_per_second': frames_per_second,
-                    'category': '%s' % class_mapping[class_id]
-                })
-
+    annotations = parse_annotations(
+        input_annotations_path, video_frames_info_path, charades_classes_path)
     with open(output, 'wb') as f:
         json.dump(annotations, f)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -93,5 +52,5 @@ if __name__ == "__main__":
         help='CSV of format <video_name>,<fps>[,<num_frames_in_video>]?')
     parser.add_argument('--output_annotation_json', required=True)
     args = parser.parse_args()
-    main(args.input_annotations, args.charades_classes, args.video_frames_info,
+    main(args.input_annotations, args.video_frames_info, args.charades_classes,
          args.output_annotation_json)
